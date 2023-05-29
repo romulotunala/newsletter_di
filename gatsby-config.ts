392d439868d956e7471a1e135268fb8d0e351068
Rômulo Tunala <romulot@ciandt.com>
2023-05-29 16:15:20 -0300

    Creating the newsletter language selection page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,9 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag"]
+  // TODO: add google analytics
+  // plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag"]
+  plugins: ["gatsby-plugin-sass"]
 };
 
 export default config;
