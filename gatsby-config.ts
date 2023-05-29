641a9a25af8f53cc4a4154ce0598a5eaaace6463
Rômulo Tunala <romulot@ciandt.com>
2023-05-29 16:54:25 -0300

    Adding gh-pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: '/newsletter_di',
   siteMetadata: {
-    title: `newsletter_d&i`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Newsletter D&I`,
+    // siteUrl: `https://romulotunala.github.io/newsletter_di/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
